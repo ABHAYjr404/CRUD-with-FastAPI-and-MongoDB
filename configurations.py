3472268ea0aa78130181b17e1fcb43c6d8cdf0fa
Abhay Manilal Chheda <abhaychheda786@gmail.com>
2025-01-15 19:44:16 +0530

    Update configurations.py

--- configurations.py
+++ configurations.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://abhaychheda:abhay007@cluster0.sojia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "paste your url from mongodb atlas"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
